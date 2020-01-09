0fa6d4c20b16798200ebf0bfb2260effecb8d392
Wishvanath SMG <anandwishvanath@gmail.com>
2020-01-09 10:06:46 +0530

    fetch all the product list using given api

--- src/App.js
+++ src/App.js
@@ -7,13 +7,47 @@ export default class App extends Component {
     this.state={
 
       //state variable
+      general_product :[],
+      rest_product : [],
+      all_product :[]
 
     }
   }
+
+
+
+  // login to categories the general product and other product
+
+
+
+
+
+
+
+
+
+
+// component did mount block
+componentDidMount(){
+  fetch('https://gist.githubusercontent.com/bharadwajturlapati/4e81154dbcc7d6928921b96057fc5b4a/raw/d31da32d6e5c1dd2a11968d7e94d3c60dfd50fcb/products.json')
+    .then(response => response.json())
+    .then(data => this.setState({all_product: data}))
+
+}
+
+
+
+
+
+
+
+
+
+// render main component
   render() {
     return (
       <div>
-        this is app component
+       {JSON.stringify(this.state.all_product)}
       </div>
     )
   }
